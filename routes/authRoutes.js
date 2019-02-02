2e5cf01393ea0a62e13696e56747f0d3b1a37832
HumbleWebDev <tom@humblewebdev.com>
2019-02-02 12:38:19 -0600

    added passport

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,27 +1,30 @@
 var models = require("../models");
-
-var passport = require('passport');
-var LocalStrategy = require('passport-local');
-
-passport.use(new LocalStrategy(
-    function(username, password, done) {
-      models.User.findOne({ username: username }, function(err, user) {
-        if (err) { return done(err); }
-        if (!user) {
-          return done(null, false, { message: 'Incorrect username.' });
-        }
-        if (!user.validPassword(password)) {
-          return done(null, false, { message: 'Incorrect password.' });
-        }
-        return done(null, user);
-      });
-    }
-  ));
+var passport = require("passport");
+var jwt = require('jsonwebtoken');
 
 module.exports = function(app) {
     app.post('/api/auth', function(request, response) {
-        passport.authenticate('local', { successRedirect: '/',
-                                   failureRedirect: '/login',
-                                   failureFlash: true })
+        passport.authenticate(
+            'local', 
+            {session: false}, 
+            function(error, user, info) {
+                if (error || !user) {
+                    return response.status(400).json({
+                        message: 'Something is not right',
+                        user   : user,
+                        error  : error
+                    });
+                }
+               request.login(user, {session: false}, (error) => {
+                   if (error) {
+                       response.send(error);
+                   }
+                   console.log("user: " + user.username);
+                   // generate a signed son web token with the contents of user object and return it in the response
+                   const token = jwt.sign(user.toJSON(), 'your_jwt_secret');
+                   response.json({user, token});
+                });
+            }
+        )(request, response);
     });
 };
--- server.js
+++ server.js
@@ -1,16 +1,51 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-var db = require("./models");
+var models = require("./models");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
+var bodyParser = require('body-parser');
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+
 
 // Middleware
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'username',
+    passwordField: 'password'
+  },
+  function(username, password, cb) {
+    models.User.findOne({ username: username }).then(
+        function(user) {
+          if (!user) { 
+            return cb(null, false, {message: 'Incorrect email or password.'});
+          }
+          return cb(null, user, {message: 'Logged In Successfully'});
+        }
+      ).catch(error => {
+        cb(error)
+      });
+    }
+  ));
 
 // Handlebars
 app.engine(
@@ -26,6 +61,16 @@ require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 require("./routes/authRoutes")(app);
 
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  models.User.findById(id, function(err, user) {
+    cb(err, user);
+  });
+});
+
 var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
@@ -35,7 +80,7 @@ if (process.env.NODE_ENV === "test") {
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
+models.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
